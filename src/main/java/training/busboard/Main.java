e595fd13b88116a91e1d91bb1ad5e2098fbcf4a8
tekzsolt <tekereszsolt@gmail.com>
2019-09-28 14:25:26 +0100

    start

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,23 @@
 package training.busboard;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
+        //id: 490008660N
+//        List<widget> widgets = client
+//                .path("/widgets")
+//                .request(MediaType.APPLICATION_JSON_TYPE)
+//                .get(new GenericType<List<Widget>>() {});
+
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+
+//        List<widget> widgets = client
+//                .path("/widgets")
+//                .request(MediaType.APPLICATION_JSON_TYPE)
+//                .get(new GenericType<List<Widget>>() {});
     }
 }	
---
+++ src/main/java/training/busboard/PEDAC.java
@@ -0,0 +1,14 @@
+package training.busboard;
+
+public class PEDAC {
+    // 1. Create user input => get stop point id
+        // 1.1 Create a User class with prompt() method
+        // 1.2 Return stop point id (String)
+    // 2. Use the stop point id to search on the TFL API
+        // 2.1 Work out what link should be targeted (include stop point id)
+    // 3. Get a response from TFL API => JSON
+        // 3.1 Map the returned JSON data to the relevant object (arrivals ?)
+            // 3.1.1 In what data structure JSON is returned ?
+            // 3.1.2 Have to map only certain data, ignore the others
+    // 4. Print off the next five buses with extra info
+}
